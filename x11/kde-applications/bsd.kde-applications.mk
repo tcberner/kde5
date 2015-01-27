c5a6a782d226b1e2b6e6d38859514bcebc596b9a
Tobias C. Berner <tcberner@gmail.com>
2015-01-27 19:25:15 +0100

    remove kde5 dependency from bsd.kde-applications.mk

--- x11/kde-applications/bsd.kde-applications.mk
+++ x11/kde-applications/bsd.kde-applications.mk
@@ -1,11 +1,15 @@
 KDE_APPLICATIONS_VERSION=	14.12.1
 MASTER_SITES?=			${MASTER_SITE_KDE}
-MASTER_SITE_SUBDIR?=		stable/applications/${KDE5_APPLICATIONS_VERSION}/src
+MASTER_SITE_SUBDIR?=		stable/applications/${KDE_APPLICATIONS_VERSION}/src
 
 PORTVERSION?=			${KDE_APPLICATIONS_VERSION}
 
+# TODO depend on docbook... (option?)
+BUILD_DEPENDS+=         ${LOCALBASE}/share/xsl/docbook/html/docbook.xsl:${PORTSDIR}/textproc/docbook-xsl \
+                        ${LOCALBASE}/share/xml/docbook/4.2:${PORTSDIR}/textproc/docbook-xml
+
+
 USE_QT5+=	qmake_build buildtools_build
-USE_KDE5+=	kdeprefix
 
 USES+=		tar:xz cmake gettext pkgconfig
 USE_LCONFIG=	yes
